30310b6004e75149dddd582f6636b2589a347fad
Graham Mace <graham@maceage.com>
2015-04-08 21:16:51 +0800

    Added island_maker.py decorators

--- module3/island_maker.py
+++ module3/island_maker.py
@@ -26,3 +26,12 @@ tracer = Trace()
 @escape_unicode
 def norwegian_island_maker(name):
     return name + 'øy'
+
+
+class IslandMaker:
+    def __init__(self, suffix):
+        self.suffix = suffix
+
+    @tracer
+    def make_island(self, name):
+        return name + self.suffix
