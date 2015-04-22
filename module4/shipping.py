7abc88ffd9d20ad89397bbdcda67cf2290dc2822
Graham Mace <graham@maceage.com>
2015-04-22 21:26:14 +0800

    Added celsius to RefrigeratedShippingContainer

--- module4/shipping.py
+++ module4/shipping.py
@@ -16,15 +16,14 @@ class ShippingContainer:
         return result
 
     @classmethod
-    def create_empty(cls, owner_code):
-        return cls(owner_code, contents=None)
+    def create_empty(cls, owner_code, *args, **kwargs):
+        return cls(owner_code, contents=None, *args, **kwargs)
 
     @classmethod
-    def create_with_items(cls, owner_code, items):
-        return cls(owner_code, contents=list(items))
+    def create_with_items(cls, owner_code, items, *args, **kwargs):
+        return cls(owner_code, contents=list(items), *args, **kwargs)
 
     def __init__(self, owner_code, contents):
-        self.owner_code = owner_code
         self.contents = contents
         self.bic = self._make_bic_code(
             owner_code=owner_code,
@@ -33,8 +32,17 @@ class ShippingContainer:
 
 class RefrigeratedShippingContainer(ShippingContainer):
 
+    MAX_CELSIUS = 4.0
+
     @staticmethod
     def _make_bic_code(owner_code, serial):
         return iso6346.create(owner_code=owner_code,
                               serial=str(serial).zfill(6),
                               category='R')
+
+    def __init__(self, owner_code, contents, celsius):
+        super().__init__(owner_code, contents)
+        if celsius > RefrigeratedShippingContainer.MAX_CELSIUS:
+            raise ValueError("Temperature too hot!")
+
+        self.celsius = celsius
