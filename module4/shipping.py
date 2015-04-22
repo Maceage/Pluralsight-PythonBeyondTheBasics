8238365969046bd27415e149666c0cd9ba2edd64
Graham Mace <graham@maceage.com>
2015-04-22 22:03:17 +0800

    Added fahrenheit to shipping.py

--- module4/shipping.py
+++ module4/shipping.py
@@ -40,9 +40,32 @@ class RefrigeratedShippingContainer(ShippingContainer):
                               serial=str(serial).zfill(6),
                               category='R')
 
+    @staticmethod
+    def _c_to_f(celsius):
+        return celsius * 9/5 + 32
+
+    @staticmethod
+    def _f_to_c(fahrenheit):
+        return (fahrenheit - 32) * 5/9
+
     def __init__(self, owner_code, contents, celsius):
         super().__init__(owner_code, contents)
-        if celsius > RefrigeratedShippingContainer.MAX_CELSIUS:
-            raise ValueError("Temperature too hot!")
-
         self.celsius = celsius
+
+    @property
+    def celsius(self):
+        return self._celsius
+
+    @celsius.setter
+    def celsius(self, value):
+        if value > RefrigeratedShippingContainer.MAX_CELSIUS:
+            raise ValueError("Temperature too hot!")
+        self._celsius = value
+
+    @property
+    def fahrenheit(self):
+        return RefrigeratedShippingContainer._c_to_f(self.celsius)
+
+    @fahrenheit.setter
+    def fahrenheit(self, value):
+        self.celsius = RefrigeratedShippingContainer._f_to_c(value)
